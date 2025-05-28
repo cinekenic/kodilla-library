55a65acabbd4fcbddb28031f749b860e19a9d9f7
Piotr-Trzcinski <cinekenic@go2.pl>
2025-05-28 17:28:05 +0200

    first commit

---
+++ src/test/java/com/kodilla/kodilla_library/KodillaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilla_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
